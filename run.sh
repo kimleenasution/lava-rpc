0e70676d06402101be063fcfa55567b57bd1d46a
Kim Lee Nasution <127822404+kimleenasution@users.noreply.github.com>
2024-04-10 08:37:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-46c67943-0fe7-42a5-bab5-2cd4d4553ba3/)
 
 
 echo "$curl_output" >> Update.tsx
